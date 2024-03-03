81ad85188a49ce2e461713d5f6266405712c5d90
Stan Skrivanek <s.skrivanek@icloud.com>
2024-03-03 11:03:25 +0000

    basic structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Footer from '$lib/components/Footer.svelte';
+	import Navigation from '$lib/components/Navigation.svelte';
 	import TemporaryNav from '$lib/components/TemporaryNav.svelte';
 	import '../app.css';
 </script>
 
-<TemporaryNav />
+<!-- <TemporaryNav /> -->
+<Navigation />
 <slot />
 <Footer />
