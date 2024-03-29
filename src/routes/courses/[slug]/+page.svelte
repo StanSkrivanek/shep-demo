cfbe380c70e6455f2bf1a4e8d677aca136067d5d
Stan Skrivanek <s.skrivanek@icloud.com>
2024-03-29 07:07:08 +0000

    CSS on SLUG PAGE

--- src/routes/courses/[slug]/+page.svelte
+++ src/routes/courses/[slug]/+page.svelte
@@ -56,12 +56,15 @@
 			<p class="small-title">Course Fee</p>
 			<div class="fee">
 				{#if full_price != 'No Charge'}
-					<p><span>In Full:</span>€ {full_price}</p>
+					<span>In Full</span>
+					<p>{full_price}</p>
 				{:else}
-					<p><span>In Full:</span>{full_price}</p>
+					<span>In Full:</span>
+					<p>{full_price}</p>
 				{/if}
 				{#if funded_price != null}
-					<p><span>Funded :</span>€ {funded_price}</p>
+					<span>Funded</span>
+					<p>{funded_price}</p>
 				{/if}
 			</div>
 		</div>
@@ -82,25 +85,24 @@
 	<div class="main__c">
 		<!-- aside -->
 		<aside>
+			<h3>Apply for</h3>
 			{#if upcoming.length == 0}
 				<div class="no-course">
 					<div class="content">
 						<p>
-							We are sorry but this course in not currently open for applications in any facilities.
+							We regret to inform you that this course is currently not available for applications
+							at any of our facilities.
 						</p>
-						<p>
-							You can sign to our newsletter to be notified when new Shep courses will open for
-							applications.
-						</p>
+						<p>Check back later or sign up to our newsletter to be notified.</p>
 					</div>
 					<div class="nlr-link">
 						<div class="dots">
-							<DotsCorner width={24} height={24} />
+							<DotsCorner width={24} height={24} currentColor="hsl(var(--hsl-white))" />
 						</div>
 						<div>
 							<p>Sign to newsletter</p>
 							<a href="/newsletter">
-								<LinkCircle width={48} height={48} />
+								<LinkCircle width={48} height={48} currentColor="hsl(var(--hsl-white))" />
 							</a>
 						</div>
 					</div>
@@ -307,36 +309,36 @@
 		grid-row: 1/3;
 		padding: 1.4rem;
 		border-radius: 1rem;
-		background: color-mix(in oklab, var(--cyan-400) 50%, white);
-		& p {
-			display: flex;
-			justify-content: space-between;
-			margin: 0;
-			color: var(--fc-main);
-			font-size: var(--sm);
-			margin-bottom: 0.5rem;
-			color: var(--fc-main);
-		}
-		& .fee {
-			font-weight: 700;
-			font-family: var(--ff-inter);
-		}
+		background: color-mix(in oklab, hsl(var(--hsl-gray) / 0.15), white);
+		text-align: center;
+
 		& .small-title {
 			position: relative;
 			margin-top: 0;
 			margin-bottom: 1.2rem;
-			font-family: var(--ff-gilroy-m);
+			font-family: 'fkg-bold', sans-serif;
 			font-size: var(--sm);
-			color: var(--fc-main);
-			width: fit-content;
+			/* width: fit-content; */
 			&::after {
 				content: '';
 				position: absolute;
 				bottom: -4px;
 				left: 0;
 				width: 100%;
-				height: 2px;
-				background: black;
+				height: 1px;
+				background: hsl(var(--hsl-gray) / 0.25);
+			}
+		}
+		& p {
+			margin: 0;
+			color: hsl(var(--hsl-gray));
+			margin-bottom: 0.25rem;
+		}
+		& .fee {
+			text-align: center;
+			font-family: 'fkg-black', sans-serif;
+			& p {
+				font-size: var(--h3);
 			}
 		}
 	}
@@ -352,21 +354,19 @@
 		justify-content: space-between;
 		padding: 1.4rem;
 		border-radius: 1rem;
-		color: var(--fc-light);
 		min-width: 160px;
-		background: color-mix(in oklab, var(--shep-orange) 20%, white);
+		background: color-mix(in oklab, hsl(var(--hsl-cyan)) 15%, white);
 		& p {
 			margin: 0;
-			color: var(--fc-main);
+
 			font-size: var(--sm);
 		}
 		& .small-title {
 			position: relative;
 			margin-top: 0;
 			margin-bottom: 1.2rem;
-			font-family: var(--ff-gilroy-m);
+			font-family: 'fkg-bold', sans-serif;
 			font-size: var(--sm);
-			color: var(--fc-main);
 			width: fit-content;
 			&::after {
 				content: '';
@@ -419,11 +419,11 @@
 		display: flex;
 		padding: 2rem;
 		border-radius: 1rem;
-		background: var(--red-200);
+		background: hsl(var(--hsl-red) / 0.8);
 		& p {
 			margin: 0;
 			margin-bottom: 0.5rem;
-			color: var(--fc-main);
+			color: hsl(var(--hsl-white));
 		}
 		& .content {
 			margin-right: 2rem;
@@ -433,7 +433,6 @@
 			flex-direction: column;
 			justify-content: space-between;
 			align-items: flex-end;
-			color: var(--red);
 			&:first-child {
 				margin-bottom: 1rem;
 			}
@@ -443,7 +442,7 @@
 				line-height: 1;
 				margin: 0;
 				margin-bottom: 0.5rem;
-				color: var(--fc-main);
+				/* color: var(--fc-main); */
 			}
 			& a {
 				display: flex;
@@ -457,7 +456,7 @@
 	}
 
 	.accordion_item {
-		--item-color: var(--shep-orange);
+		--item-color: hsl(var(--hsl-brand));
 		--_base-color-private: var(--item-color, var(--clr-base));
 		/* changing value of `--item-color` will change colors of each child element that contain `--_base-color-private`  */
 		margin-bottom: 1rem;
@@ -670,16 +669,16 @@
 			grid-row: 1/2;
 		}
 		.hero-data {
-			grid-column: 1/7;
-			grid-row: 3/5;
+			grid-column: 1/5;
+			grid-row: 2/5;
 		}
 		.hero-sm-box-top {
-			grid-column: 7/-1;
-			grid-row: 3/4;
+			grid-column: 5/7;
+			grid-row: 2/5;
 		}
 		.hero-sm-box-bottom {
 			grid-column: 7/-1;
-			grid-row: 4/5;
+			grid-row: 2/5;
 		}
 		.accordion .accordion-links a {
 			margin-bottom: 1rem;
@@ -707,16 +706,10 @@
 		.no-course {
 			display: block;
 		}
-	}
-
-	@media (max-width: 768px) {
-		.hero-img {
-			grid-column: 1/-1;
-			grid-row: 1/2;
-		}
 		.hero-data {
 			grid-column: 1/-1;
 			grid-row: 2/3;
+			padding: 1rem;
 		}
 		.hero-sm-box-top {
 			grid-column: 1/5;
@@ -726,6 +719,13 @@
 			grid-column: 5/-1;
 			grid-row: 3/5;
 		}
+	}
+
+	@media (max-width: 768px) {
+		/* .hero-img {
+			grid-column: 1/-1;
+			grid-row: 1/2;
+		} */
 	}
 	@media (max-width: 576px) {
 		.main__c {
